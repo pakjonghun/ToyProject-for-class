884625953eae22d9389a104b25bf9959f47a69ef
정훈박 <fireking5997@gmail.com>
2021-12-18 23:08:59 +0900

    cache clean

--- dist/PageComponent.js
+++
@@ -1,6 +0,0 @@
-export default class PageComponent {
-    render(parent, data) {
-        parent.innerHTML = `<span>${data.desc}</span>`;
-    }
-}
-//# sourceMappingURL=PageComponent.js.map
--- dist/components/page.js
+++
@@ -1,8 +0,0 @@
-import { BasicComponent } from "./basic.js";
-export class PageComponent extends BasicComponent {
-    constructor() {
-        super("ul", "list");
-        this.element.textContent = "this is test page";
-    }
-}
-//# sourceMappingURL=page.js.map
