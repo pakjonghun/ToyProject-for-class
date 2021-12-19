449ae4fe28b74b61ac458c5e5dcd8e32f601f95a
정훈박 <fireking5997@gmail.com>
2021-12-19 15:41:41 +0900

    Add:regExp 연습

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -3,7 +3,6 @@ import { Component } from "../../component.js";
 export class VideoComponent extends Component<HTMLElement> {
   constructor(url: string, desc: string) {
     const readToInsertUrl = parseToEmbededUrl(url);
-    console.log(readToInsertUrl);
     super(`<div><iframe class="video"></iframe><p class="desc"></p></div>`);
     const video = this.element.querySelector(".video")! as HTMLIFrameElement;
     const content = this.element.querySelector(
@@ -15,10 +14,15 @@ export class VideoComponent extends Component<HTMLElement> {
 }
 
 function parseToEmbededUrl(rawUrl: string): string {
-  if (rawUrl.includes("embed")) return rawUrl;
-  const temp = rawUrl.split("/");
-  const id: string = temp[temp.length - 1]! as string;
-  return `https://www.youtube.com/embed/${
-    id.includes("?") ? id.split("=").slice(-1)[0] : id
-  }`;
+  const regExp =
+    /(?:https?\:\/\/)?(?:www\.)?(?:youtu(?:be)?\.(?:com\/)?(?:be\/)?(?:embed\/)?(?:watch(?:\=)?)?(?:\?v=)?(?:\?d\=)?)?([a-zA-z0-9]{11})/;
+
+  const temp = rawUrl.match(regExp);
+  const id = temp && temp[1];
+  return !!id ? `https://www.youtube.com/embed/${id}` : rawUrl;
 }
+
+// https://www.youtube.com/embed/VIDEO_ID
+// <iframe id="ytplayer" type="text/html" width="640" height="360"
+//   src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+//   frameborder="0"></iframe>
