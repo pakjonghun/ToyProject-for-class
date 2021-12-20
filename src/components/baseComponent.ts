10a2928449706b256093adcc0b0ea0b7f018295b
정훈박 <fireking5997@gmail.com>
2021-12-20 19:16:39 +0900

    Add:베이스 컴포넌트 작성

---
+++ src/components/baseComponent.ts
@@ -0,0 +1,15 @@
+export interface IbaseComponent {
+  attachTo(parent: HTMLElement, position: InsertPosition): void;
+}
+
+export class BaseComponent implements IbaseComponent {
+  protected readonly element: HTMLElement;
+  constructor(htmlString: string) {
+    const template = document.createElement("template");
+    template.innerHTML = htmlString;
+    this.element = template.content.firstElementChild! as HTMLElement;
+  }
+  attachTo(parent: HTMLElement, position: InsertPosition = "beforeend") {
+    parent.insertAdjacentElement(position, this.element);
+  }
+}
